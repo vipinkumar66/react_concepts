3d6fd815ab0821cba1c415bdc2c282db2650612c
vipin <vipinkumarlamba55@gmail.com>
2023-03-31 20:40:03 +0530

    props

--- src/index.js
+++ src/index.js
@@ -2,35 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const firstBook = {
+  title : "Dog Man",
+  author : "Dav Pilkey" ,
+  img : "./assets/dogman.jpg"
+}
+
+const secondBook = {
+  title : "Outlive",
+  author : "Peter Attia MD" ,
+  img : "./assets/outlive.jpg"
+}
+
+const thirdBook = {
+  title : "How to Catch the Easter Bunny",
+  author : "Adam Wallace" ,
+  img : "./assets/bunny.jpg"
+}
+
 const BookList = () => {
   return (
     <section className='booklist'>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
+        <Book title = {firstBook.title}
+         author = {firstBook.author}
+         img = {firstBook.img}>
+          <button>Click me</button>
+         </Book>
+
+        <Book title = {secondBook.title}
+         author = {secondBook.author}
+         img = {secondBook.img}/>
+
+        <Book title = {thirdBook.title}
+         author = {thirdBook.author}
+         img = {thirdBook.img}/>
     </section>
   )
 }
 
-const Book = () =>{
+
+const Book = (props) =>{
+  const {title, author, img, children} = props
     return(
         <article className='book'>
-            <Image/>
-            <Title/>
-            <Author/>
+            <img src={img} alt={title}/>
+            <h2>{title}</h2>
+            <h2>{author}</h2>
+            {children}{/*  here the children is a special prop */}
         </article>
     )
 }
-
-const Image = () => <img src='./assets/dogman.jpg'alt='Dog Man'/>
-const Title = () => {
-    return(
-        <h2>Dog Man</h2>
-    )
-}
-const Author = () => <h3>Dav Pilkey</h3>
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<BookList/>);
