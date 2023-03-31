47b736af94524f561c9e8400b84540b66d359af8
vipin <vipinkumarlamba55@gmail.com>
2023-03-31 17:16:58 +0530

    Booklist project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () => {
+  return (
+    <div>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </div>
+  )
+}
+
+const Book = () =>{
+    return(
+        <article>
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>
+    )
+}
+
+const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg'alt='Dog Man'/>
+const Title = () => {
+    return(
+        <h2>Dog Man</h2>
+    )
+}
+const Author = () => <h3>Dav Pilkey</h3>
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList/>);
+
