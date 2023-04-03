7d2b5131695e606cbb3abdde3932a341a53fdb8b
vipin <vipinkumarlamba55@gmail.com>
2023-04-03 16:41:07 +0530

    Events

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
@@ -6,38 +6,69 @@ const books =[
   {
     title : "Dog Man",
     author : "Dav Pilkey" ,
-    img : "./assets/dogman.jpg"
+    img : "./assets/dogman.jpg",
+    id:1
   },
 
   {
     title : "Outlive",
     author : "Peter Attia MD" ,
-    img : "./assets/outlive.jpg"
+    img : "./assets/outlive.jpg",
+    id:2
   },
 
   {
     title : "How to Catch the Easter Bunny",
     author : "Adam Wallace" ,
-    img : "./assets/bunny.jpg"
+    img : "./assets/bunny.jpg",
+    id:3
   }
 ]
 
 const BookList = () => {
   return (
-    <section className='booklist'>
-      {books.map((book) => {
-        return(
-          <article className='book'>
-            <img src={book.img} alt={book.title}/>
-            <h2>{book.title}</h2>
-            <h2>{book.author}</h2>
-          </article>
-        )
-      })};
-    </section>
+    <div>
+      <FormComponent/>
+
+      <section className='booklist'>
+        {books.map((book) => {
+          return(
+            <article className='book' key={book.id}>
+              <img src={book.img} alt={book.title}/>
+              <h2>{book.title}</h2>
+              <button onClick={()=> console.log(book.title)}>Click here</button>
+              <h2>{book.author}</h2>
+            </article>
+          )
+        })};
+      </section>
+    </div>
    )
 }
 
+const FormComponent = () =>{
+  const [value, setValue] = useState('')
+
+  const handleDefault = (event) =>{
+    event.preventDefault();
+    console.log(`Form submitted with the ${value}`)
+
+  }
+
+  const handleChange = (event) =>{
+    setValue(event.target.value)
+  }
+
+  return(
+    <section className='submitform'>
+      <form onSubmit={handleDefault} className='sectionform'>
+        <input placeholder='Enter the book name here..' type='text' value={value} onChange={handleChange}/>
+        {/* here in the onClick part that is the anonynmous callback function */}
+        <button type='submit' onClick={() => console.log("Button Clicked")}>Submit</button>
+      </form>
+    </section>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<BookList/>);
